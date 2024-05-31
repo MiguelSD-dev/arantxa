6ea813bc3d4576717bd9883757e99e35a77adb57
miceica <miguelceica23@gmail.com>
2024-05-31 16:39:13 +0200

    Viernes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-function loadPartials() {
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
   fetch('partials/header.html')
     .then(response => response.text())
     .then(data => {
       document.querySelector('header').innerHTML = data;
+      var path = window.location.pathname;
+      var page = path.split("/").pop();
+
+      if (page === ""){
+        page = "index.html";
+      }
+
+      var navLinks = document.querySelectorAll('.nav-link');
+      navLinks.forEach(function (link) {
+        if (link.getAttribute('href') === page) {
+          link.classList.add('active');
+        } 
+      });
     });
 
   fetch('partials/footer.html')
@@ -10,6 +26,10 @@ function loadPartials() {
     .then(data => {
       document.querySelector('footer').innerHTML = data;
     });
-}
 
-document.addEventListener('DOMContentLoaded', loadPartials);
+
+
+
+
+
+});
